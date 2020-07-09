19d69326d3f24b451c4ea0052047ca54f7107a17
NormanBenbrahim <norman.benbrahim@gmail.com>
2020-07-08 22:40:07 -0400

    main sample file setup for gcloud

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import datetime
-
 from flask import Flask, render_template
+from google.cloud import datastore
+
+datastore_client = datastore.Client()
+
+###### functions for datastore
+def store_time(dt):
+    entity = datastore.Entity(key=datastore_client.key('visit'))
+    entity.update({
+        'timestamp': dt
+    })
+
+    datastore_client.put(entity)
+
+
+def fetch_times(limit):
+    query = datastore_client.query(kind='visit')
+    query.order = ['-timestamp']
+
+    times = query.fetch(limit=limit)
+
+    return times
+####### end functions for datastore
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    # store access time in datastore
+    store_time(datetime.datetime.now())
 
-    return render_template('index.html', times=dummy_times)
+    # fetch most 10 recent access times from datastore
+    times = fetch_times(10)
+
+    return render_template('index.html', times=times)
 
 
 if __name__ == '__main__':
