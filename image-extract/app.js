010b2c6768190dd61a6a20df497b92176e303505
NormanBenbrahim <norman.benbrahim@gmail.com>
2020-10-24 19:04:48 -0400

    finishing touches

--- image-extract/app.js
+++ image-extract/app.js
@@ -17,7 +17,7 @@ if (!fs.existsSync('../data/images')){
     fs.mkdirSync('../data/images');
 }
 
-// download files if they're base64 strings, or urls
+// download files if they're base64 strings or urls
 async function download(url_src, dest) { 
     // check if the url starts with http or if it's a base64 string
     if (url_src.startsWith('http')) {
@@ -38,27 +38,6 @@ async function download(url_src, dest) {
     return true
 }
 
-// scroll to bottom of screen when on google images page results
-// async function scrollToBottom(page, itemTargetCount=1000) {
-//     let items = [];
-//     try {
-//         let previousHeight;
-//         while (items.length <= itemTargetCount) {
-//             items = await page.evaluate(() => Array.from(document.images, e => e.src));
-//             previousHeight = await page.evaluate('document.body.scrollHeight');
-//             await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-//             await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
-//             await page.waitForNavigation();
-//         }
-//     } 
-//     catch(e) { 
-//         console.log(e) 
-//     }
-
-//     return true;
-// }
-
-
 // for a list of valid queries for searching google this article rocks:
 // https://stenevang.wordpress.com/2013/02/22/google-advanced-power-search-url-request-parameters/
 
@@ -67,19 +46,22 @@ async function image_extract(query) {
     // parse the query to work with urls
     const url_query = await query.split(' ').join('+')
     const output_folder = await query.split(' ').join('_')
+    const path = `../data/images/${output_folder}`
 
-    // check if output folder already exists
-    const path = '../data/images/' + output_folder
-    let exists
-    fs.access(path, (err) => {
-        exists = false
-    });
+    // check if output folder already exists, if so exit 
+    if (!fs.existsSync(path)){
+        fs.mkdirSync(path);
+    }
+    else {
+        console.log(`WARN:  ${path} already exists, exiting program`)
+        process.exit(0)
+    }
 
     // launch the browser
     const browser = await puppeteer.launch({
         headless: false,
-        defaultViewport: null,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        defaultViewport: null/*,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],*/
     });
     const page = await browser.newPage();
     page.setViewport({width: 1280, height: 926})
@@ -97,32 +79,22 @@ async function image_extract(query) {
     const scroll_finish = await scrollPageToBottom(page, 250, 1000) // should return true
     await page.waitForTimeout()
 
-    if (exists==false) {
-        // create the folder
-        await fs.mkdir(path, (err) => {console.log(err)})
-        // get a list of urls
-        const images = await page.evaluate(() => Array.from(document.images, e => e.src))
-        var result // scope
-        for (let i = 0; i < images.length; i++) {
-            if (i!=399) {
-                result = await download(images[i], `${path}/img-${i}.png`)
+    // get a list of urls
+    const images = await page.evaluate(() => Array.from(document.images, e => e.src))
+    var result // scope
+    for (let i = 0; i < images.length; i++) {
+        result = await download(images[i], `${path}/img-${i}.png`)
 
-                if (result === true) {
-                    console.log('Success:', `${path}/img-${i}.png`, 'has been downloaded successfully.')
-                } 
-                else {
-                    console.log('Error:', `${path}/img-${i}.png`, 'was not downloaded.')
-                    console.error(result)
-                }                
-            }
-        }
+        if (result === true) {
+            console.log('Success:', `${path}/img-${i}.png`, 'has been downloaded successfully.')
+        } 
+        else {
+            console.log('Error:', `${path}/img-${i}.png`, 'was not downloaded.')
+            console.error(result)
+        }                       
+    }
 
-        await browser.close()
-    }
-    else {
-        console.log('folder already exists, exiting')
-        await browser.close()
-    }
+    await browser.close()
 
 }
 
