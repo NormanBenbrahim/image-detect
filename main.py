21c5d1a2aba542e101f0f42e902a584121ad54c4
NormanBenbrahim <norman.benbrahim@gmail.com>
2020-10-24 14:35:38 -0400

    model function initialized

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
-import re, os, sys, datetime, time
-import pandas
+import os
+import tensorflow as tf 
 
-# if data & logs folders don't exist, create them
-if not os.path.exists('./data/'):
-    os.makedirs('./data')
-
-if not os.path.exists('./data/train-model/'):
-    os.makedirs('./data/train-model')
-
-if not os.path.exists('./data/logs/'):
-    os.makedirs('./data/logs')
-
-# initialize api key
-# TODO: add to start.sh & start-local.sh
-#gcs_creds = os.environ['GCS_DEVELOPER_KEY']
-#gcs_project = os.environ['GCS_CX']
+# define number of classes based on number of folders in image directory
+path = './data/images'
+number_classes = sum(os.path.isdir(os.path.join(path, i)) for i in os.listdir(path))
 
 
+# define the model
+def fcn_model(dropout_rate=0.2):
+    
+    # input layer set to (None,None) to allow for variable inputs
+    input_layer = tf.keras.layers.Input(shape=(None, None, 3))
 
- 
-if __name__ == '__main__':
-    print("main")
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, strides=1)(input_layer)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=1)(x)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=2)(x)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    x = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=2)(x)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    x = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=2)(x)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    # Fully connected layer 1
+    x = tf.keras.layers.Conv2D(filters=64, kernel_size=1, strides=1)(x)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    # Fully connected layer 2
+    x = tf.keras.layers.Conv2D(filters=number_classes, kernel_size=1, strides=1)(x)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.GlobalMaxPooling2D()(x)
+    predictions = tf.keras.layers.Activation('softmax')(x)
+
+    model = tf.keras.Model(inputs=input_layer, outputs=predictions)
+    
+    print(model.summary())
+    print(f'Total number of layers: {len(model.layers)}')
+
+    return model
+
+fcn_model()
